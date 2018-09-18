b31082882440ba7b04014ebc154514d50cc96b6b
Malcome Fein <mfeinflatiron@gmail.com>
2018-09-18 15:55:56 -0400

    Created Star Wars Program

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,10 @@
 def welcome
   # puts out a welcome message here!
+  "Welcome to Star Wars Encyclopedia"
 end
 
 def get_character_from_user
-  puts "please enter a character"
+  puts "Please enter a character"
+  user_response = gets.chomp.downcase
   # use gets to capture the user's input. This method should return that input, downcased.
 end
