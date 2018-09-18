52b01e7ba6684d6f2c94643799c07013db5d84a6
github username <github email address>
2018-09-18 19:25:15 -0400

    adds Res-Client

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,9 +6,10 @@ def get_character_movies_from_api(character)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
-  
-  # NOTE: in this demonstration we name many of the variables _hash or _array. 
+
+  # NOTE: in this demonstration we name many of the variables _hash or _array.
   # This is done for educational purposes. This is not typically done in code.
+
 
 
   # iterate over the response hash to find the collection of `films` for the given
@@ -30,6 +31,7 @@ def show_character_movies(character)
   films_array = get_character_movies_from_api(character)
   print_movies(films_array)
 end
+
 
 ## BONUS
 
