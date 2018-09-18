6d503c05edb0719a06972cbcbc7506bfc6348e4f
shemarjoseph <shemarjoseph@yahoo.com>
2018-09-18 14:11:49 -0400

    finish api code

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,7 +2,7 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character)
+def get_character_movies_from_api(character_name)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
@@ -15,6 +15,26 @@ def get_character_movies_from_api(character)
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
   #  for that film
+  films = []
+  response_hash["results"].each do |characters|
+    if characters["name"].downcase == character_name
+      characters["films"].each do |film|
+        films << film
+      end
+    end
+  end
+
+  binding.pry
+
+  result = []
+  films.each do |film|
+    result << JSON.parse(RestClient.get(film))
+  end
+
+  result
+
+
+
   # return value of this method should be collection of info about each film.
   #  i.e. an array of hashes in which each hash reps a given film
   # this collection will be the argument given to `parse_character_movies`
@@ -24,6 +44,10 @@ end
 
 def print_movies(films_hash)
   # some iteration magic and puts out the movies in a nice list
+  films_hash.each_with_index do |film_info, index|
+    puts film_info
+    puts "#{index + 1}. #{film_info["title"]}"
+  end
 end
 
 def show_character_movies(character)
